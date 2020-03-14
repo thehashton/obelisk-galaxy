450642d19245565df321a99eae5c5eaf24cd6df0
Harry Ghazni <hghazni@gmail.com>
2020-03-14 16:36:14 +0000

    fixes production build for netlify

---
+++ build/precache-manifest.fb3e3585521c596c281e4501a611889a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb9166977ef358430c5e",
+    "url": "/static/css/main.880d0054.chunk.css"
+  },
+  {
+    "revision": "fb9166977ef358430c5e",
+    "url": "/static/js/main.fb916697.chunk.js"
+  },
+  {
+    "revision": "cc03315f6cdca6b5ba84",
+    "url": "/static/js/1.cc03315f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4f90f7ae0b3000068723824673e3b7fa",
+    "url": "/index.html"
+  }
+];
